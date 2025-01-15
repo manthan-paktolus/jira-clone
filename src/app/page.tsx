bfe1500521dc819ecfddc6d8a8862fe04206fd43
Your Name <you@example.com>
2025-01-15 20:19:02 +0530

    auth screen design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
-import Test from "@/features/Test";
 
 export default function Home() {
   return (
     <div>
-      {/* <Button>Click me</Button>
-      <p className="text-red-500 font-semibold">Manthan</p> */}
-      <Test />
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
--- src/features/Test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Test = () => {
-  return <div className="text-yellow-500">Test</div>;
-};
-
-export default Test;
